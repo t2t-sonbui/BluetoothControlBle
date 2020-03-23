0706a00a32a91bcb10a3b94281214ac3095598dd
Disnaka Bandara <h.p.bandara@cqumail.com>
2020-03-23 13:27:21 +0700

    Initial commit Tut: https://vidieukhien.xyz/series/lap-trinh-esp32-voi-arduino/

---
+++ app/src/main/java/com/demo/embedded/bluetoothcontroll/Constants.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.demo.embedded.bluetoothcontroll;
+
+/**
+ * Defines several constants used between {@link BluetoothSPPService} and the UI.
+ */
+public interface Constants {
+
+    // Message types sent from the BluetoothChatService Handler
+    int MESSAGE_STATE_CHANGE = 1;
+    int MESSAGE_READ = 2;
+    int MESSAGE_WRITE = 3;
+    int MESSAGE_DEVICE_NAME = 4;
+    int MESSAGE_TOAST = 5;
+
+    // Key names received from the BluetoothChatService Handler
+    String DEVICE_NAME = "device_name";
+    String TOAST = "toast";
+    //
+    public static final String CRLF = "\r\n";
+    public static final String CMD_RF_1_ON = "ON";
+    public static final String CMD_RF_1_OFF = "OFF";
+    // Rec
+    public static final String REC_1_ON = "ON";
+    public static final String REC_1_OFF = "OFF";
+
+
+}
